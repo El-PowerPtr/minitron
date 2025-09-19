00079aef93579a31c0fca3c6ea6f2f616d923a1e
El-PowerPtr <moiseselpower@gmail.com>
2025-09-19 11:04:36 -0400

    feat/nodes/in: InRef type and receive method of In created

--- src/nodes/input.rs
+++ src/nodes/input.rs
@@ -1,3 +1,12 @@
+use std::{
+    boxed::Box,
+    rc::Rc,
+    cell::RefCell
+};
+
 pub (mod) trait In {
-    fn comp_in(in: &Vec<f32>) -> f32;
+    fn comp_in(inp: &Vec<f32>) -> f32;
+    fn recieve(x: f32);
 }
+
+type InRef<N: In> = Rc<RefCell<Box<N>>>;
