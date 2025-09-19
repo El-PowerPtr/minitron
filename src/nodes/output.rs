aea248edc8dcee37e3a83cb11e0aefa7d5247e63
El-PowerPtr <moiseselpower@gmail.com>
2025-09-19 17:07:40 -0400

    feat/nodes/out: Connect trait created

--- src/nodes/output.rs
+++ src/nodes/output.rs
@@ -1,9 +1,12 @@
+use super::input::*;
+
 pub (mod) trait Out{
-    fn activation(&self) -> f32;
+    fn activation(x: f32) -> f32;
 }
 
 pub (mod) trait Connect<N: In>{
-    fn connect(&mut self, weight: f32, node: &N) -> bool;
+    fn connect(&mut self, weight: f32, node: InRef<N>);
+    fn forward_prop(&self, f32);
 }
 
 #[inline]
