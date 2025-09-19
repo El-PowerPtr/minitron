4c6141f42a2dcebd0601c8cf9eea5188ceac365e
El-PowerPtr <moiseselpower@gmail.com>
2025-09-19 13:00:19 -0400

    feat/nodes/in: InputNode struct created# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## Date:      Fri Sep 19 13:00:19 2025 -0400## On branch main# Changes to be committed:#	modified:   src/nodes/input.rs## Changes not staged for commit:#	modified:   src/nodes/output.rs#

--- src/nodes/input.rs
+++ src/nodes/input.rs
@@ -5,8 +5,24 @@ use std::{
 };
 
 pub (mod) trait In {
-    fn comp_in(inp: &Vec<f32>) -> f32;
+    fn comp_in(&self) -> f32;
     fn recieve(x: f32);
 }
 
 type InRef<N: In> = Rc<RefCell<Box<N>>>;
+
+pub (mod) struct InputNode<N: In>: In + Out + Connect {
+    next_nodes: Vec<(f32, InRef<N>)>,
+}
+
+impl <N: In> In for InputNode<N> {
+    fn activation(x: f32) -> f32 {
+        x
+    }
+}
+
+impl <N: In> Connect<N> for InputNode<N> {
+    fn connect(&mut self, weight: f32, node: InRef<N>) {
+        self.next_nodes.push(node.clone())
+    }
+}
