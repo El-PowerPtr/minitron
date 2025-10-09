ae52a7af47a698ba030c9c6c3aa23e9ab4b31095
El-PowerPtr <moiseselpower@gmail.com>
2025-10-09 17:01:39 -0400

    feat/label/training_label: training label struct created

--- src/nodes/label.rs
+++ src/nodes/label.rs
@@ -1,5 +1,17 @@
-use super::input::In;
+use super::{
+    input::In,
+    output::Out,
+    learn::Learn,
+};
+use crate::MultiRef;
+
 
 pub trait Label<T>: In {
     fn label(&self) -> T;
+}
+
+pub struct TrainingLabel<N:Learn + Out, T>{
+    label: T,
+    expected_val: f32,
+    related_node:  MultiRef<N>,
 }
