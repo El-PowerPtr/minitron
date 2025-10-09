e26ce67ff55b31851309ad9be5f0fbde9f16ef43
El-PowerPtr <moiseselpower@gmail.com>
2025-10-08 22:37:28 -0400

    feature/layers: fresh constructor added

--- src/layers/layer.rs
+++ src/layers/layer.rs
@@ -1,7 +1,9 @@
 use crate::{
     learning_rate::LearningRateManager,
+    SharedRef,
 };
 
 pub trait Layer <N, L: LearningRateManager>{
     fn link<T>(&mut self, other: &mut impl Layer<T, L>);
+    fn fresh(learning_manager: SharedRef<N>) -> Self;
 }
