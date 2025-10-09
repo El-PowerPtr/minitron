0fd6d0e9fa961df51c907fe19d1e9eca8abb15a3
El-PowerPtr <moiseselpower@gmail.com>
2025-10-09 18:20:00 -0400

    feat/layers/hidden_layer: learn method imolemented for hidden layer

--- src/layers/hidden_layer.rs
+++ src/layers/hidden_layer.rs
@@ -11,7 +11,16 @@ use crate::{
 
 use super::layer::*;
 
-struct HiddenLayer<N: In + Out + BackProp, M: LearningRateManager>{
+struct HiddenLayer<N: In + Out + Learn + BackProp, M: LearningRateManager>{
     learning_manager: SharedRef<M>,
     nodes: Vec<MultiRef<N>>,
+}
+
+impl <N: In + Out + Learn + BackProp, M: LearningRateManager> HiddenLayer<N,M>{
+    fn learn(&mut self){
+        for i in &mut self.nodes {
+            i.borrow_mut().learn();
+            i.borrow_mut().send_feedback();
+        }
+    }
 }
